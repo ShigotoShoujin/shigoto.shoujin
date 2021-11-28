d3b00b845040e0a367397dd647b66843511a1005
Shigoto Shoujin <shigoto.shoujin@outlook.com>
2021-11-28 18:47:08 -0500

    Grid Test - IterableIterator

--- src/grid/iterable_iterator.hpp
+++ src/grid/iterable_iterator.hpp
@@ -1,32 +1,34 @@
-//#pragma once
-//#include "iterable.hpp"
-//#include "forward_iterator.hpp"
-//
-//template<typename T>
-//class IterableIterator : public ForwardIterator<T>, public Iterable<T>
-//{
-//public:
-//	IterableIterator(T* iterator_begin, int iterator_step, T* iterable_begin, T* iterable_end) :
-//		ForwardIterator<T>{ iterator_begin, iterator_step },
-//		Iterable<T>{iterable_begin, iterable_begin}
-//	{}
-//
-//	T* begin()
-//	{
-//		_sync_range();
-//		return Iterable<T>::begin();
-//	}
-//
-//	T* end()
-//	{
-//		_sync_range();
-//		return Iterable<T>::end();
-//	}
-//
-//private:
-//	void _sync_range()
-//	{
-//		auto&& a = ForwardIterator<T>::operator->();
-//		Iterable<T>::MoveRange(a);
-//	}
-//};
+#pragma once
+#include "iterable.hpp"
+#include "forward_step_iterator.hpp"
+
+namespace shoujin {
+
+template<typename T>
+class IterableIterator : public ForwardStepIterator<T>, public Iterable<T> {
+public:
+	IterableIterator(T* begin, T* end, int step) :
+		ForwardStepIterator<T>{begin, step},
+		Iterable<T>{begin, end}
+	{}
+
+	T* begin()
+	{
+		_sync_range();
+		return Iterable<T>::begin();
+	}
+
+	T* end()
+	{
+		_sync_range();
+		return Iterable<T>::end();
+	}
+
+private:
+	void _sync_range()
+	{
+		Iterable<T>::SetBegin(ForwardStepIterator<T>::operator->());
+	}
+};
+
+}
