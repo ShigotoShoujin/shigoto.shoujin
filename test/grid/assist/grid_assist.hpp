cd6d0452313f84c8413d04612eb2713d11a15f8c
Shigoto Shoujin <shigoto.shoujin@outlook.com>
2021-11-30 23:40:03 -0500

    Grid - GridAssist hpp/cpp

--- test/grid/assist/grid_assist.hpp
+++ test/grid/assist/grid_assist.hpp
@@ -1,6 +1,5 @@
 #include "CppUnitTest.h"
 #include <array>
-#include <cassert>
 #include <vector>
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
@@ -9,18 +8,8 @@ namespace assist {
 
 class GridVectorAsserter {
 public:
-	GridVectorAsserter(int width, int height) :
-		_width{width},
-		_height{height},
-		_expected{height}
-	{
-		assert(_height > 0);
-	}
-
-	void Push(int row, int value)
-	{
-		_expected[row].push_back(value);
-	}
+	GridVectorAsserter(int width, int height);
+	void Push(int row, int value);
 
 	template<int Size>
 	void AssertSameAsArray(const std::array<int, Size> array)
@@ -33,12 +22,7 @@ private:
 	int _width, _height;
 	std::vector<std::vector<int>> _expected;
 
-	void _assert_all_rows_same_size()
-	{
-		auto size = _expected[0].size();
-		for(auto& row : _expected)
-			Assert::AreEqual(size, row.size());
-	}
+	void _assert_all_rows_same_size();
 
 	template<int Size>
 	void _assert_match_array(const std::array<int, Size> array)
@@ -47,13 +31,7 @@ private:
 			Assert::IsTrue(_compare_row(_expected[i].data(), array.data() + i * _width));
 	}
 
-	bool _compare_row(const int* actual, const int* expected)
-	{
-		bool result = false;
-		for(int i = 0; i < _width; ++i, ++actual, ++expected)
-			result |= *actual == *expected;
-		return result;
-	}
+	bool _compare_row(const int* actual, const int* expected);
 };
 
 template<int Size>
